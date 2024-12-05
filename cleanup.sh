5352c73d5450426c3a890dbf6b27a938253afe17
helwan-linux <152768713+helwan-linux@users.noreply.github.com>
2024-12-05 13:34:07 +0200

    Helwan Linux Educational Cinnamon V10.1
    Helwan Linux Educational Cinnamon V10.1
    used archiso version 82

---
+++ cleanup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#set -e
+
+echo "out folder and work folder will be cleaned"
+echo "==============================="
+sudo rm -rvf out/ 
+sudo rm -rvf work/
+echo "==============================="
+echo "CLEANED ......"
+echo "==============================="
